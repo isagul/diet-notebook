e118cb1a7b6bd984174b0c6c9ed0208a6fbda80f
Isa Gul <isa.gul@getir.com>
2023-01-04 16:51:00 +0300

    update apple-touch-icon path

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link rel="apple-touch-icon" href="/apple-icon-57x57.png"></link>
           <meta name="theme-color" content="#5D3EBC" />
           <meta
             name="viewport"
